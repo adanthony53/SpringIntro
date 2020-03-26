c289ceb69bd9baf176612cf7543463cad10bcf13
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-03-26 15:39:57 -0700

    Updated written IoC

--- src/main/java/com/spring_intro/ioc/ClassPathXmlApplicationContext.java
+++ src/main/java/com/spring_intro/ioc/ClassPathXmlApplicationContext.java
@@ -4,10 +4,11 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
-import org.xml.sax.SAXException;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
@@ -30,7 +31,73 @@ public class ClassPathXmlApplicationContext implements ApplicationContext {
                 // Get Constructor with no params
                 Constructor constructor = classes.getConstructor();
                 Object object = constructor.newInstance();
-                ioc.put(id, object);
+
+                // Set initial default values for the object
+                Iterator<Element> beanIter = element.elementIterator();
+                while (beanIter.hasNext()) {
+                    Element prop = beanIter.next();
+                    String name = prop.attributeValue("name");
+                    String valueStr = prop.attributeValue("value");
+                    String ref = prop.attributeValue("ref");
+
+                    if (ref == null) {
+                        String methodName = "set" + name.substring(0,1).toUpperCase() + name.substring(1);
+                        Field field = classes.getDeclaredField(name);
+                        Method method = classes.getDeclaredMethod(methodName, field.getType());
+
+                        // change the type of value
+                        Object value = null;
+                        if (field.getType().getName() == "long") {
+                            value = Long.parseLong(valueStr);
+                        } else if (field.getType().getName() == "int") {
+                            value = Integer.parseInt(valueStr);
+                        } else if (field.getType().getName() == "java.lang.String") {
+                            value = valueStr;
+                        }
+                        method.invoke(object, value);
+                    }
+                    ioc.put(id, object);
+                }
+            }
+
+            // do direct injection
+            Iterator<Element> iter2 = root.elementIterator();
+            while (iter2.hasNext()) {
+                Element element = iter2.next();
+                String id = element.attributeValue("id");
+                String className = element.attributeValue("class");
+                // Use Reflection Mechanism to create Object
+                Class classes = Class.forName(className);
+                // Get Constructor with no params
+                Constructor constructor = classes.getConstructor();
+                Object object = constructor.newInstance();
+
+                // Set initial default values for the object
+                Iterator<Element> beanIter = element.elementIterator();
+                while (beanIter.hasNext()) {
+                    Element prop = beanIter.next();
+                    String name = prop.attributeValue("name");
+                    String valueStr = prop.attributeValue("value");
+                    String ref = prop.attributeValue("ref");
+
+                    String methodName = "set" + name.substring(0,1).toUpperCase() + name.substring(1);
+                    Field field = classes.getDeclaredField(name);
+                    Method method = classes.getDeclaredMethod(methodName, field.getType());
+                    Object value = null;
+
+                    // change the type of value
+                    if (field.getType().getName() == "long") {
+                        value = Long.parseLong(valueStr);
+                    } else if (field.getType().getName() == "int") {
+                        value = Integer.parseInt(valueStr);
+                    } else if (field.getType().getName() == "java.lang.String") {
+                        value = valueStr;
+                    } else if (field.getType().getName() == "com.spring_intro.entity.Address") {
+                        value = ioc.get("address");
+                    }
+                    method.invoke(object, value);
+                    ioc.put(id, object);
+                }
             }
 
         } catch (DocumentException e) {
@@ -44,6 +111,8 @@ public class ClassPathXmlApplicationContext implements ApplicationContext {
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
     }
