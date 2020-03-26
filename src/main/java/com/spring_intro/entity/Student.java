bcd1f90c9ce7d19197589c4c7d06ae82747d1a2c
adanthony53 <32273951+adanthony53@users.noreply.github.com>
2020-03-26 13:44:27 -0700

    Minor bug fixed

--- src/main/java/com/spring_intro/entity/Student.java
+++ src/main/java/com/spring_intro/entity/Student.java
@@ -1,8 +1,12 @@
 package com.spring_intro.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private long id;
     private String name;
